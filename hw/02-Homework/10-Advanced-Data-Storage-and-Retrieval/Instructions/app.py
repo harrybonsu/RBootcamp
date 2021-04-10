fc648a296e22548fc1bb60652398269fba324eb2
Harry Bonsu <75817600+harrybonsu@users.noreply.github.com>
2021-04-09 21:50:32 -0400

    updated hw files

--- hw/02-Homework/10-Advanced-Data-Storage-and-Retrieval/Instructions/app.py
+++ hw/02-Homework/10-Advanced-Data-Storage-and-Retrieval/Instructions/app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -35,7 +36,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     print("Listing all available APIs")
-    
+
     return (f"Welcome to the Home Page!</br>"
             f"Below is a list of the climate APIs available:</br>"
             f"/api/v1.0/precipitation</br>"
@@ -45,6 +46,111 @@ def index():
             f"/api/v1.0/<start>/<end>"
             )
 
+
+## List all routes that are available.
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all precipitations
+    dates = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    results =  session.query(Measurement.date, func.avg(Measurement.prcp)).\
+            filter(Measurement.date.between (dates, dt.date(2017, 8, 23))).group_by('date')
+
+    session.close()
+
+    precipitation_list = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["Date"] = date
+        precipitation_dict["Precipitation"] = prcp
+        precipitation_list.append(precipitation_dict)
+
+    return jsonify(precipitation_list)
+
+## Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def stations():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    results = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    stations_list = []
+    for station in results:
+        station_dict = {"Station":station[0],
+                        "Station Name":station[1]}
+        stations_list.append(station_dict)
+
+    return jsonify(stations_list)
+
+## Query the dates and temperature observations of the most active station for the last year of data.
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    dates = dt.date(2017, 8, 23) - dt.timedelta(days=365)    
+
+    # Query all stations
+       
+    results = session.query(Measurement.station, func.count(Measurement.date)).group_by(Measurement.station).\
+    order_by(func.count(Measurement.date).desc()).all()
+
+    session.close()
+
+    most_active_station = []
+    for tobs in results:
+         temp_dict={"Date":tobs[0],
+                    "Temperature":tobs[1]
+        }
+    most_active_station.append(temp_dict)
+
+    return jsonify(most_active_station)
+
+## Return a JSON list of temperature observations (TOBS) for the previous year.
+
+@app.route('/api/v1.0/<start>', defaults={'end': None})
+@app.route("/api/v1.0/<start>/<end>")
+def determine_temps_for_date_range(start, end):
+   
+    # Create our session (link) from Python to the DB.
+    session = Session(engine)
+
+    # Both a start date and an end date.
+    if end != None:
+        temperature_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).filter(
+            Measurement.date <= end).all()
+    # Start date only.
+    else:
+        temperature_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).all()
+
+    session.close()
+
+    # Convert the query results to a list.
+    temperature_list = []
+    no_temperature_data = False
+    for min_temp, avg_temp, max_temp in temperature_data:
+        if min_temp == None or avg_temp == None or max_temp == None:
+            no_temperature_data = True
+        temperature_list.append(min_temp)
+        temperature_list.append(avg_temp)
+        temperature_list.append(max_temp)
+    # Return the JSON representation of dictionary.
+    if no_temperature_data == True:
+        return f"No temperature data found for the given date range. Try another date range."
+    else:
+        return jsonify(temperature_list)
 
 
 if __name__=="__main__":
