e823bc870c54ea2133f44e5d9f478e3dcee89cc5
Harry Bonsu <75817600+harrybonsu@users.noreply.github.com>
2021-05-06 12:49:02 -0400

    updated html hw

--- hw/02-Homework/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ hw/02-Homework/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
+//console.log(tableData);
 
-// YOUR CODE HERE!
+// Selecting the table body
+var tbody = d3.select("table")
+tbody.attr("table table-striped")
+
+// Loop Through `data` and console.log each weather report object
+tableData.forEach(function(ufoSightings) {
+    //console.log(ufoSightings);
+    var row = tbody.append('tr')
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+    //console.log(key, value);
+    var cell = row.append('td');
+    cell.text(value);
+    
+
+    });
+})
+
+
+
