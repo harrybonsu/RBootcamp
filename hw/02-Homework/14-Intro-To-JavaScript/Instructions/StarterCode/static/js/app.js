a9c0c68176f24195b1f34e0a6cd98b73f5d0a575
Harry Bonsu <75817600+harrybonsu@users.noreply.github.com>
2021-05-06 14:51:59 -0400

    updated html hw

--- hw/02-Homework/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ hw/02-Homework/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -6,7 +6,9 @@ var tableData = data;
 var tbody = d3.select("table")
 tbody.attr("table table-striped")
 
-// Loop Through `data` and console.log each weather report object
+// Looping Through `data` and appending a table to the web page with new 
+//rows of data for each UFO sighting.
+
 tableData.forEach(function(ufoSightings) {
     //console.log(ufoSightings);
     var row = tbody.append('tr')
@@ -17,7 +19,46 @@ tableData.forEach(function(ufoSightings) {
     
 
     });
-})
+});
+
+// Using the date form to listen for events to search through the 'date/time' 
+// column for user input
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the input element
+var inputField = d3.select("#datetime");
+
+// Select the form
+var form = d3.select("form")
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputField = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputField.property("value");
+
+    console.log(inputValue)
+
+    var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+
+    console.log(filteredData)
+
+    
+
+}
+
 
 
 
