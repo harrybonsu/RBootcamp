1468864133eaedce4bb8010a0104f9df462bfaf2
Harry Bonsu <75817600+harrybonsu@users.noreply.github.com>
2021-05-27 13:55:27 -0400

    updatetd files

--- hw/02-Homework/17-Mapping-Web/Instructions/static/js/logic.js
+++ hw/02-Homework/17-Mapping-Web/Instructions/static/js/logic.js
@@ -1,124 +1,123 @@
 var earthQuakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
 
 // Perform a GET request to the query URL
-d3.json(earthQuakeUrl, function(data) {
+d3.json(earthQuakeUrl).then(function (data) {
     // Once we get a response, send the data.features object to the createFeatures function
+    console.log(data);
     createFeatures(data.features);
-  });
-  
-  function createFeatures(earthquakeData) {
-  
+});
+
+function createFeatures(earthquakeData) {
+
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<h4> Magnitude: " + feature.properties.mag +"</h4>");
+        layer.bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<h4> Magnitude: " + feature.properties.mag + "</h4>");
     }
-  
-  
-  // Function for Circle Color Base on Criteria. The Color Scale is base of the 7 colors of a Rainboy ROY G BIV
-  function QuakeColor(Qcolor) {
-      switch(true) {
-          case (0 <= Qcolor && Qcolor <= 1.0):
-            return "Red";
-          case (1.0 <= Qcolor && Qcolor <= 2.0):
-              return "Orange";
-          case (2.0 <= Qcolor && Qcolor<= 3.0):
-            return "Yellow";
-          case (3.0 <= Qcolor && Qcolor<= 4.0):
-              return "Green";
-          case (4.0 <= Qcolor && Qcolor<= 5.0):
-              return "Blue";
-          case (5.0 <= Qcolor && Qcolor <= 6.0):
-            return "Indigo";
-          default:
-            return "Violet";
-      }
+    // Function for Circle Color Base on Criteria. The Color Scale is base of the 7 colors of a Rainboy ROY G BIV
+    function QuakeColor(color) {
+        switch (true) {
+            case (color <= 0 && color <= 1.0):
+                return "Red";
+            case (color <= 1.0 && color <= 2.0):
+                return "Orange";
+            case (color <= 2.0 && color <= 3.0):
+                return "Yellow";
+            case (color <= 3.0 && color <= 4.0):
+                return "Green";
+            case (color <= 4.0 && color <= 5.0):
+                return "Blue";
+            case (color <= 5.0 && color <= 6.0):
+                return "Purple";
+            default:
+                return "Pink";
+        }
     }
-  //   Create a circle function
-  
-  function CircleMaker(features, latlng){
-      var CircleOptions = {
-          radius: features.properties.mag * 8,
-          fillColor: QuakeColor(features.properties.mag),
-          color: QuakeColor(features.properties.mag),
-          opacity: 1.0,
-          fillOpacity: .5
-  
-      }
-      return L.circleMarker(latlng, CircleOptions)
-  }
+    //   Create a circle function
+
+    function CircleMaker(features, latlng) {
+        var CircleOptions = {
+            radius: features.properties.mag * 8,
+            fillColor: QuakeColor(features.properties.mag),
+            color: QuakeColor(features.properties.mag),
+            opacity: 1.0,
+            fillOpacity: .5
+
+        }
+        return L.circleMarker(latlng, CircleOptions)
+    }
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature,
-      pointToLayer: CircleMaker
+        onEachFeature: onEachFeature,
+        pointToLayer: CircleMaker
     });
-  
+
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
-  }
-  
-  function createMap(earthquakes) {
-  
+}
+
+function createMap(earthquakes) {
+
     // Define streetmap and Satellite layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
     });
-  
+
     var Satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "satellite-v9",
-      accessToken: API_KEY
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-v9",
+        accessToken: API_KEY
     });
-  
-  
+
+
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/light-v10",
-      accessToken: API_KEY
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/light-v10",
+        accessToken: API_KEY
     });
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
-      "Street Map": streetmap,
-      "Light Map": lightmap,
-      "Satellite Map": Satellite
+        "Street Map": streetmap,
+        "Light Map": lightmap,
+        "Satellite Map": Satellite
     };
-  
+
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
-      Earthquakes: earthquakes
+        Earthquakes: earthquakes
     };
-  
+
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("mapid", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [streetmap, earthquakes]
+        center: [
+            37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [streetmap, earthquakes]
     });
-  
-  // Create a legend to display information about our map
-  var info = L.control({
-      position: "bottomright"
+
+    // Create a legend to display information about our map
+    var info = L.control({
+        position: "bottomright"
     });
-  // Add the info legend to the map
-  info.addTo(myMap);
-  
+    // Add the info legend to the map
+    info.addTo(myMap);
+
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
     // Add the layer control to the map
     L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
+        collapsed: false
     }).addTo(myMap);
-  }
+}
